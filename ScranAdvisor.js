6970d5eccf3060c1075480585bec8f798374003b
Billie Redwood <billieredwood@gmail.com>
2023-11-22 16:54:11 +0000

    initial commit

---
+++ ScranAdvisor.js
@@ -0,0 +1,32 @@
+const ScranAdvisor = function(restaurants){
+    this.restaurants = restaurants;
+}
+
+// Add methods to prototype
+
+
+
+//count the number of restaurants
+ScranAdvisor.prototype.totalRestaurants = function () {
+    return this.restaurants.length
+}
+
+// can find the name of restaurant by name
+ScranAdvisor.prototype.findByName = function (name) {
+    return this.restaurants.find(restaurant => restaurant.name === name);
+}
+
+// can find the name of all restaurants
+ScranAdvisor.prototype.findAllNames = function (names) {
+    return this.restaurants.name;
+}
+
+//can find all restaurants from Glasgow
+ScranAdvisor.prototype.findRestaurantByCity = function (town) {
+    return this.restaurants.filter(restaurant => restaurant.location.town === "Glasgow");
+}
+
+
+
+
+module.exports = ScranAdvisor;
